84ccfcf67dae09067afa013305edc5b2b720534e
Shurui <srliu3264@gmail.com>
2025-11-24 14:00:39 -0800

    configure autosave.lua, keybind <localleader>sa to turn on/off autosave.

--- lua/autosave.lua
+++ lua/autosave.lua
@@ -1,12 +1,51 @@
--- autosave for every 1 second
-local timer = vim.loop.new_timer()
-timer:start(
-  1000, -- wait 1 seconds
-  1000, -- repeat every 1 seconds
-  vim.schedule_wrap(function()
-    local buf = vim.api.nvim_get_current_buf()
-    if vim.api.nvim_buf_get_option(buf, 'modified') then
-      vim.cmd 'silent write'
+local autosave_group = vim.api.nvim_create_augroup('Autosave', { clear = true })
+
+-- 1. The Autocommand Logic
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+  group = autosave_group,
+  pattern = '*',
+  callback = function()
+    -- Safety Checks: Do not save special buffers (fixes E382) or empty names
+    if vim.bo.buftype ~= '' or not vim.bo.modifiable or vim.fn.expand '%' == '' then
+      return
     end
-  end)
-)
+
+    -- Check User Preference
+    if vim.b.autosave_enabled == nil then
+      -- Ask the user (once per buffer)
+      local choice = vim.fn.confirm(
+        'Enable Autosave for ' .. vim.fn.expand '%:t' .. '?',
+        '&Yes\n&No',
+        2 -- Default to No
+      )
+
+      if choice == 1 then
+        vim.b.autosave_enabled = true
+        print 'Autosave Enabled'
+      else
+        vim.b.autosave_enabled = false
+        print 'Autosave Disabled'
+      end
+    end
+
+    -- Execute Save if enabled
+    if vim.b.autosave_enabled == true and vim.bo.modified then
+      vim.cmd 'silent! write'
+    end
+  end,
+})
+
+-- 2. The Toggle Keymap (<Leader>as)
+vim.keymap.set('n', '<Leader>sa', function()
+  if vim.b.autosave_enabled then
+    vim.b.autosave_enabled = false
+    print 'Autosave: DISABLED for this buffer'
+  else
+    vim.b.autosave_enabled = true
+    -- Force a write immediately if we just enabled it and the file is dirty
+    if vim.bo.modified then
+      vim.cmd 'silent! write'
+    end
+    print 'Autosave: ENABLED for this buffer'
+  end
+end, { desc = 'Toggle Autosave for current buffer' })
