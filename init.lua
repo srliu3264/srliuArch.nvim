bd56224240633f40136b744f8cb3b5a85d229fb1
Shurui <srliu3264@gmail.com>
2025-12-02 18:42:23 -0800

    install plugin to run cpp and set F6 to build&run

--- init.lua
+++ init.lua
@@ -218,6 +218,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- keybind vista to see TOC structure of current buffer
 vim.keymap.set('n', '<leader>tv', ':Vista!!<CR>')
 
+-- NOTE: Cpp keymaps
+-- F6: Save, Build, AND Run immediately
+vim.keymap.set('n', '<F6>', ':w <bar> !cmake --build build && ./build/main<CR>', { desc = 'Build and Run' })
+
 -- NOTE: LATEX setups
 
 -- NOTE: molten ipykernel setups:
@@ -860,7 +864,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
---
+++ lua/custom/plugins/debug.lua
@@ -0,0 +1,34 @@
+return {
+  -- Add this to your plugins table or a separate file like lua/plugins/debug.lua
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      require('mason-nvim-dap').setup {
+        ensure_installed = { 'codelldb' }, -- Installs the C++ debugger
+        handlers = {},
+      }
+
+      dapui.setup()
+
+      -- Keymaps (Standard Kickstart usually handles this, but just in case)
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+
+      -- Auto open/close debug UI
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    end,
+  },
+}
